5168d544549e588756271f260e8778961376fe9d
frandrioli-maker <frandrioli@gmail.com>
2026-05-03 21:12:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js - Service Worker básico
+self.addEventListener('install', (e) => {
+  console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
